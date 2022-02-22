982687799551285d08abfb1f80aab47f847f45b9
Isilielelf <100211976+Isilielelf@users.noreply.github.com>
2022-02-22 16:41:09 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="App">
+     <a href="https://hunterxhunter.fandom.com/ru/wiki/%D0%9A%D0%B8%D0%BB%D0%BB%D1%83%D0%B0_%D0%97%D0%BE%D0%BB%D0%B4%D0%B8%D0%BA">Киллуа Золдик</a>
+      <div className="block1">
+      <a href="https://hunterxhunter.fandom.com/ru/wiki/%D0%98%D0%BB%D0%BB%D1%83%D0%BC%D0%B8_%D0%97%D0%BE%D0%BB%D0%B4%D0%B8%D0%BA">Иллуми Золдик</a>
+    <div className="block2">
+        <a href="https://kimetsu-no-yaiba.fandom.com/ru/wiki/%D0%A2%D0%B5%D0%BD%D0%B3%D0%B5%D0%BD_%D0%A3%D0%B7%D1%83%D0%B9">Тенген Узуй</a>
+    <div className="block3">
+        <a href="https://kimetsu-no-yaiba.fandom.com/ru/wiki/%D0%94%D0%B0%D0%BA%D0%B8">Даки</a>
+    </div>
+      </div>
+      </div>
+      </div>
+  );
+}
+
+export default App;
